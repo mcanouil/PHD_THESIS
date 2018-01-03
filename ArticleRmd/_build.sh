d258935d7699d30188eaa188e0e2f2fb38f4c0ed
mcanouil <8896044+mcanouil@users.noreply.github.com>
2018-01-03 15:25:02 +0100

    Update 20170103 ArticleRmd
    Signed-off-by: Mickaël Canouil <8896044+mcanouil@users.noreply.github.com>

--- ArticleRmd/_build.sh
+++ ArticleRmd/_build.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-Rscript -e "bookdown::render_book('index.Rmd', 'bookdown::pdf_document2')"
-Rscript -e "bookdown::render_book('index.Rmd', 'bookdown::word_document2')"
+Rscript -e "bookdown::render_book(input = 'index.Rmd', output_format = 'bookdown::pdf_document2', encoding = 'UTF-8')"
+Rscript -e "bookdown::render_book(input = 'index.Rmd', output_format = 'bookdown::word_document2', encoding = 'UTF-8')"
