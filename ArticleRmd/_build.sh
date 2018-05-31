a1f86d6685b974a540686021b8a2b9152dac4095
mcanouil <8896044+mcanouil@users.noreply.github.com>
2018-05-31 16:52:49 +0200

    remove all rmd article
    Signed-off-by: Mickaël Canouil <8896044+mcanouil@users.noreply.github.com>

--- ArticleRmd/_build.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-
-Rscript -e "bookdown::render_book(input = 'index.Rmd', output_format = 'bookdown::pdf_document2', encoding = 'UTF-8')"
-Rscript -e "bookdown::render_book(input = 'index.Rmd', output_format = 'bookdown::word_document2', encoding = 'UTF-8')"
